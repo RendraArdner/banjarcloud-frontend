98745d4d540638712f89e65f09a519c54399b24a
RendraArdner <rendra.the3rd@gmail.com>
2026-06-07 18:42:22 -0700

    Create App component for BanjarCloud Store

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function App() {
+  return (
+    <div style={{fontFamily:"Arial",textAlign:"center",padding:"50px"}}>
+      <h1>BanjarCloud Store</h1>
+      <h2>Kalimantan Selatan Tech Hub</h2>
+      <p>Deployed with AWS Amplify | LKS 2026</p>
+  </div>
+  );
+}
+export default App;
